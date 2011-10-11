49d25c3773b20e74d00c4affc61316697f51e858
Jonathan Reed <rjonat2@vt.edu>
2011-10-11 14:23:36 -0400

    minor changes, added original v0.8 client to interceptor

--- udp_off.sh
+++ udp_off.sh
@@ -1,8 +1,8 @@
-echo "Blocking UDP traffic..."
+echo "Blocking UDP traffic to port 5000..."
 sleep 1
 sudo iptables -F
-sudo iptables -A INPUT -p udp -j DROP
-sudo iptables -A OUTPUT -p udp -j DROP
-sudo iptables -A FORWARD -p udp -j DROP
+sudo iptables -A INPUT -p udp --dport 5000 -j DROP
+#sudo iptables -A OUTPUT -p udp --dport 5000 -j DROP
+#sudo iptables -A FORWARD -p udp --dport 5000 -j DROP
 echo "done."
 
